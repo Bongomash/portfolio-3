c9f89455b6be06a843f0d9e3dd785ba184095809
Marc H <marcdahlhendriksen@gmail.com>
2024-10-07 15:13:46 +0200

    first asingment of javacript and fixed som html

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 'use strict';
+//check if user input matches random number
+
+//makeing the random number between 1-20
+
+const randomNumber = Math.floor(Math.random() * 20) + 1;
+console.log(randomNumber);
+
+// check if the random number and the input matches
+
+//declare var
+const buttonCheck = document.querySelector(".btnCheck")
+const guessInput = document.querySelector(".guess");
+const scoreDisplay = document.querySelector(".score");
+const highscoreDisplay = document.querySelector(".highscore");
+let scoreValue = 0;
+let highscore = Infinity;
+function checkInputMatchesNumber () {
+    const userGuess = Number(guessInput.value);
+
+    if (userGuess === randomNumber){
+        console.log("true");
+        if (scoreValue < highscore) {
+            highscore = scoreValue; //
+            highscoreDisplay.textContent = highscore;
+        }
+        guessInput.value = '';
+        return true;
+
+    }
+else  {
+    console.log("false");
+        scoreValue++;
+        guessInput.value = '';
+        scoreDisplay.textContent = scoreValue;
+        return false;
+
+    }
+}
+// eventlistener to execute the code
+buttonCheck.addEventListener("click", checkInputMatchesNumber);
+
+
+
+
